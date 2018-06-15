07072c16008460accfff9a70f4532329422d215c
Leandro Souza <l.souzadsantos@gmail.com>
2018-06-15 12:38:56 -0300

    Pequenas alterações

---
+++ Models/FavList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GithubAPI.Models
+{
+    public class FavList
+    {
+		public string nome { get; set; }
+		public string id { get; set; }
+    }
+}
